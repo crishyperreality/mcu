2d3081c67054dbf9cc6f5c772b1086521e38475c
Cris <crismalm98@gmail.com>
2021-07-08 17:02:04 +0200

    nuevos estilos banner

--- mcu/src/app/movies/movies-list/movies-list.component.ts
+++ mcu/src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Movies } from 'src/app/model/movie';
 import { MoviesService } from '../movies.service';
 import { HttpClient } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { Movie } from '../../model/movie';
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.scss']
 })
-export class MoviesListComponent implements OnInit, OnChanges {
+export class MoviesListComponent implements OnInit {
 
   @Output() selectedMovie = new EventEmitter<Movie>();
   movie: Movie;
@@ -18,36 +18,19 @@ export class MoviesListComponent implements OnInit, OnChanges {
   active: boolean = false;
   constructor(private moviesService: MoviesService, httpClient: HttpClient) { }
 
-  ngOnChanges(): void {
-    if (this.release === false){
-      console.log('uno')
-      this.moviesService.viewChronologyOrder()
-        .subscribe((movies)=>{
-          console.log(movies)
-          this.movies = movies;
-        })
-    } else {
-      console.log('dos')
-      this.moviesService.viewReleaseOrder()
-        .subscribe((movies)=>{
-        console.log(movies)
-        this.movies = movies;
-      })
-    }
-  }
-
   ngOnInit(): void {
+    console.log('hola!')
     this.moviesService.viewReleaseOrder()
     .subscribe((movies)=>{
       console.log(movies)
       this.movies = movies;
+      console.log(this.movies)
     })
   }
 
   showMovie(movie: Movie): void {
     this.movie = movie;
     this.active = true;
-    // this.selectedMovie.emit(movie);
   }
 
 }
